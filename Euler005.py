81f1841f3d731555f2bebe9451035904473af261
AB95 <aboucher598@gmail.com>
2015-10-09 02:10:22 +0100

    Cleaned up problem 5

--- Euler005.py
+++ Euler005.py
@@ -1,19 +1,16 @@
-import time
+def euler5(n):
+    if n == 1:
+        return 1
+    elif n > 1:
+        num = n * (n - 1)
+        while not is_divisible(n, num):
+            num += n * (n - 1)
 
-@profile
-def func():
-    start = time.time()
-    u = 20
-    finished = False
-    x = [19, 18, 17, 16, 15, 14, 13, 12, 11]
-    
-    while not finished:
-        if u%x[0]==0 and u%x[1]==0 and u%x[2]==0 and u%x[3]==0 and u%x[4]==0 and u%x[5]==0 and u%x[6]==0 and u%x[7]==0 and u%x[8]==0:
-             print u
-             finished = True
-        else:
-            u += 20
-    end = time.time()
-    print end-start
+        return num
 
-func()
+
+def is_divisible(n, num):
+    for i in xrange(1, n+1):
+        if num % i != 0:
+            return False
+    return True
