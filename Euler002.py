8a5c359a20a5f189c57e1bcd9b970f7fe1265eb8
AB95 <aboucher598@gmail.com>
2015-10-09 01:48:19 +0100

    Cleaned up problem 3

--- Euler002.py
+++ Euler002.py
@@ -3,7 +3,7 @@ def euler2(n):
     b = 1
     total = 0
     while b < n:
-        a, b = b, a+b
-        if b%2 == 0:
+        a, b = b, a + b
+        if b % 2 == 0:
             total += b
     return total
--- Euler003.py
+++ Euler003.py
@@ -1,40 +1,19 @@
-import time
-from math import sqrt
+def euler3(n):
+    largest = 0
+    while n % 2 == 0:
+        largest = 2
+        n /= 2
 
-def func():
-    start = time.time()
-    x = 600851475143
-    n = 4
-        
-    def isprime(f):
-        a = 4
-        squrt = sqrt(f)
-        if f%2 == 0:
-            a = 4
-            return False
-        if f%3 == 0:
-            a = 4
-            return False
-        while a<=squrt:
-            if f%a == 0:
-                a = 4
-                return False
-            a+=1
-        a = 4
-        return True
-    
-    while n<=x+1/2:
-        if n%2 == 0:
-            n += 1
-        elif n%3 == 0:
-            n += 1
-        elif x%n == 0 and isprime(n):
-            x = x/n
-            print n
-            n += 1
+    i = 3
+    while n > 1:
+        if n % i == 0:
+            if i > largest:
+                largest = i
+            n /= i
         else:
-            n += 1
-    end = time.time()
-    print end-start
-    
-func()
+            i += 2
+
+    if n > largest:
+        largest = n
+
+    return largest
