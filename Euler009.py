80870faf8e44725ce83c9ce5b2f04e0de46db1cf
AB95 <aboucher598@gmail.com>
2015-10-28 21:18:38 +0000

    Cleaned up problem 9

--- Euler009.py
+++ Euler009.py
@@ -1,28 +1,8 @@
-import time
+def func(n):
+    for b in xrange(1, n):
+        for a in xrange(1, b):
+            c = n - (a + b)
+            if a**2 + b**2 == c**2:
+                return a*b*c
 
-def func():
-    start = time.time()
-    a = 1
-    b = 1
-    c = 1
-    
-    while c<1000:
-        if a < b and b < c and a+b+c == 1000 and a**2 + b**2 == c**2:
-            print str(a) + ", " + str(b) + ", " + str(c)
-        else:
-            if b-a<200:
-                if c-b < 200:
-                    c += 1
-                    if a < b and b < c and a+b+c == 1000 and a**2 + b**2 == c**2:
-                        print str(a) + ", " + str(b) + ", " + str(c)
-                        print a*b*c
-                        break
-                else: 
-                    c = b
-                    b += 1
-            else:
-                b = a
-                a += 1
-    end = time.time()
-    print end-start
-func()
+print func(1000)
